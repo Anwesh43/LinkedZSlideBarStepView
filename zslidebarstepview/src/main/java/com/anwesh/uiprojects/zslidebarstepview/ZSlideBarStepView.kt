72fe62fbe806a9187c8758535c951be3592827db
Anwesh43 <anweshthecool0@gmail.com>
2018-12-15 11:05:11 +0530

    adding ZSBSNode class

--- zslidebarstepview/src/main/java/com/anwesh/uiprojects/zslidebarstepview/ZSlideBarStepView.kt
+++ zslidebarstepview/src/main/java/com/anwesh/uiprojects/zslidebarstepview/ZSlideBarStepView.kt
@@ -117,4 +117,49 @@ class ZSlideBarStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class ZSBSNode(var i : Int, val state : State = State()) {
+
+        private var next : ZSBSNode? = null
+
+        private var prev : ZSBSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = ZSBSNode(0)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawZSBNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : ZSBSNode {
+            var curr : ZSBSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
