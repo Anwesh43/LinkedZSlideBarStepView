22d31e033611f062f7f30448dc6e17c24f10ee5b
Anwesh43 <anweshthecool0@gmail.com>
2018-12-15 11:23:48 +0530

    adding view ina activity

--- app/src/main/java/com/anwesh/uiprojects/linkedzslidebarstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedzslidebarstepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedzslidebarstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.zslidebarstepview.ZSlideBarStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        ZSlideBarStepView.create(this)
     }
 }
