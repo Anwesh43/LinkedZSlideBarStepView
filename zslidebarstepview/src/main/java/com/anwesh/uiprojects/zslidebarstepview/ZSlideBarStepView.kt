869dd9b6b976a9b81e5c1b411bed03de7c444e80
Anwesh43 <anweshthecool0@gmail.com>
2018-12-15 11:01:42 +0530

    adding Animator class

--- zslidebarstepview/src/main/java/com/anwesh/uiprojects/zslidebarstepview/ZSlideBarStepView.kt
+++ zslidebarstepview/src/main/java/com/anwesh/uiprojects/zslidebarstepview/ZSlideBarStepView.kt
@@ -89,4 +89,32 @@ class ZSlideBarStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class Animator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch (ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
