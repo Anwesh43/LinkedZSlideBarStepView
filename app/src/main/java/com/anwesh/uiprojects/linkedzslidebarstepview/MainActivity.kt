078c330c20152f96fab259694898e732ed16e0cc
Anwesh43 <anweshthecool0@gmail.com>
2018-12-15 11:24:26 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedzslidebarstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedzslidebarstepview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedzslidebarstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.zslidebarstepview.ZSlideBarStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         ZSlideBarStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
