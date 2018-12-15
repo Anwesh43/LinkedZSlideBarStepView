0b571cac3db72b6c089ec7cde7990e20eac40082
Anwesh43 <anweshthecool0@gmail.com>
2018-12-15 10:46:44 +0530

    adding Float and Int extension methods

--- zslidebarstepview/src/main/java/com/anwesh/uiprojects/zslidebarstepview/ZSlideBarStepView.kt
+++ zslidebarstepview/src/main/java/com/anwesh/uiprojects/zslidebarstepview/ZSlideBarStepView.kt
@@ -19,3 +19,13 @@ val sizeFactor : Int = 6
 val color : Int = Color.parseColor("#3498db")
 val scGap : Float = 0.05f
 val scDiv : Double = 0.51
+
+fun Int.getInverse() : Float = 1f / this
+
+fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.getInverse(), Math.max(0f, this - i * n.getInverse())) * n
+
+fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
+
+fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor() * b.getInverse()
+
+fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = dir * scGap * mirrorValue(a, b)
