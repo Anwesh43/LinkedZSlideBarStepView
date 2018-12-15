a2cf9517935187086dd0810d3b710f8374886606
Anwesh43 <anweshthecool0@gmail.com>
2018-12-15 10:55:20 +0530

    adding ZSlideBarStepView class

--- zslidebarstepview/src/main/java/com/anwesh/uiprojects/zslidebarstepview/ZSlideBarStepView.kt
+++ zslidebarstepview/src/main/java/com/anwesh/uiprojects/zslidebarstepview/ZSlideBarStepView.kt
@@ -52,3 +52,21 @@ fun Canvas.drawZSBNode(i : Int, scale : Float, paint : Paint) {
     }
     restore()
 }
+
+class ZSlideBarStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
