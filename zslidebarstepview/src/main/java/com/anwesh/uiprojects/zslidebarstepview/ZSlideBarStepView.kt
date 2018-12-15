b83d835ff8fbaa22b6b6403a6ae85099e0f627f0
Anwesh43 <anweshthecool0@gmail.com>
2018-12-15 10:43:08 +0530

    importing libraries and defining constants

--- zslidebarstepview/src/main/java/com/anwesh/uiprojects/zslidebarstepview/ZSlideBarStepView.kt
+++ zslidebarstepview/src/main/java/com/anwesh/uiprojects/zslidebarstepview/ZSlideBarStepView.kt
@@ -1,5 +1,21 @@
 package com.anwesh.uiprojects.zslidebarstepview
 
+import android.graphics.Color
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.RectF
+import android.app.Activity
+import android.content.Context
+
 /**
  * Created by anweshmishra on 15/12/18.
  */
+
+val nodes : Int = 5
+val bars : Int = 2
+val sizeFactor : Int = 6
+val color : Int = Color.parseColor("#3498db")
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
