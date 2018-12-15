f56a8f35e35c17cc8cff2881ae174c9b6cea82e7
Anwesh43 <anweshthecool0@gmail.com>
2018-12-15 10:53:27 +0530

    adding drawZSBNode canvas extension method

--- zslidebarstepview/src/main/java/com/anwesh/uiprojects/zslidebarstepview/ZSlideBarStepView.kt
+++ zslidebarstepview/src/main/java/com/anwesh/uiprojects/zslidebarstepview/ZSlideBarStepView.kt
@@ -15,7 +15,7 @@ import android.content.Context
 
 val nodes : Int = 5
 val bars : Int = 2
-val sizeFactor : Int = 6
+val sizeFactor : Int = 4
 val color : Int = Color.parseColor("#3498db")
 val scGap : Float = 0.05f
 val scDiv : Double = 0.51
@@ -29,3 +29,26 @@ fun Float.scaleFactor() : Float = Math.floor(this / scDiv).toFloat()
 fun Float.mirrorValue(a : Int, b : Int) : Float = (1 - scaleFactor()) * a.getInverse() + scaleFactor() * b.getInverse()
 
 fun Float.updateScale(dir : Float, a : Int, b : Int) : Float = dir * scGap * mirrorValue(a, b)
+
+
+fun Canvas.drawZSBNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = w / (nodes + 1)
+    val size : Float = gap / sizeFactor
+    val sc1 : Float = scale.divideScale(0, 2)
+    val sc2 : Float = scale.divideScale(1, 2)
+    val wGap : Float = size / (bars + 1)
+    save()
+    translate(gap * (i + 1), h/2)
+    rotate(90f * sc2)
+    drawRect(-wGap/2, -size/2, wGap/2, size/2, paint)
+    for (j in 0..(bars - 1)) {
+        val sc : Float = sc1.divideScale(j, bars)
+        save()
+        translate(-wGap * (1 - 2 * j), -(1 - 2 * j) * size * sc)
+        drawRect(RectF(-wGap/2, -size/2, wGap/2, size/2), paint)
+        restore()
+    }
+    restore()
+}
